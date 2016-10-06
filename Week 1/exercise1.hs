9040965d1a4401d4d29dc88b4e64843e105f37e8
mdipirro <matteodipirro@gmail.com>
2016-10-06 23:46:10 +0200

    Add toDigitsRev and correct toDigits

--- Week 1/exercise1.hs
+++ Week 1/exercise1.hs
@@ -1,4 +1,8 @@
 toDigits :: Integer -> [Integer]
 toDigits x
-    | x < 10    = [x]
-    | otherwise = toDigits (x `div` 10) ++ [x - 10 * (x `div` 10)]
+  | x <= 0    = []
+  | x < 10    = [x]
+  | otherwise = toDigits (x `div` 10) ++ [x - 10 * (x `div` 10)]
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev x = reverse (toDigits x)
