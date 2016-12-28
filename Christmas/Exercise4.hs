1be82836e519f58ed152b16d88ec69d2e2302503
mdipirro <matteodipirro@gmail.com>
2016-12-28 17:05:01 +0100

    Add comments and write more functional code

--- Christmas/Exercise4.hs
+++ Christmas/Exercise4.hs
@@ -2,7 +2,16 @@ fibs :: [Integer]
 fibs =  base ++ (next base)
         where base = [0, 1]
 
+{-|
+`f`is the (n+1)-th Fibonacci's number. Starting from a list containing n Fibonacci's
+numbers, `next` computes the next number in a lazy way.
+`uncurry (+)` gives as a result something like [(\(a, b) -> a + b)]
+`last` takes the last pair of the list
+`zip fs $ tail fs` gives a list of pair like [(n, n-1)], where `n` and `n-1` are
+the n-th and the (n-1)-th Fibonacci's numbers.
+The (n+1)-th number is computed iff the computation is necessary. Otherwise the
+computation stops.
+-}
 next :: [Integer] -> [Integer]
 next fs = f : next (fs ++ [f])
-          where sum (a, b)  = a + b
-                f           = sum $ last $ zip fs $ tail fs
+          where f = uncurry (+) $ last $ zip fs $ tail fs
